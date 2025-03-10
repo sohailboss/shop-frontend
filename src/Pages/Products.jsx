c97db14e8479c340deda083b9bce3eecd393414b
Mohitgh123 <mohithiremathmohit@gmail.com>
2025-03-10 10:08:04 +0530

    done

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -111,12 +111,12 @@ export default function Products() {
               onChange={handleSearch}
               className="px-4 py-2 border border-gray-300 rounded-md mr-4 focus:outline-none focus:ring-2 focus:ring-indigo-500"
             />
-            <Link
+            <Link>
               href="/product/add"
               className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700"
-            >
+    
               <Plus className="h-5 w-5 mr-2" />
-              Add New Product
+            search
             </Link>
           </div>
         </div>
